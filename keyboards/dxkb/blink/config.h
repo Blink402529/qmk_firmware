2197385b4bc20178443e0f613b8abbef3fe3d47c
DEUX <deuxetienne2@gmail.com>
2025-03-07 13:00:35 +0100

    feat: add double tap to dxkb

--- keyboards/dxkb/blink/config.h
+++ keyboards/dxkb/blink/config.h
@@ -1,2 +1,4 @@
 // Allows to use uf2-split-left and uf2-split-right
 #define EE_HANDS
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
