f7dea87d7d7cf457bd6de6aa6e2a864b6733ba3f
DEUX <deuxetienne2@gmail.com>
2026-05-09 12:40:05 +0200

    feat: [dxkb] new keyboard layout

--- keyboards/dxkb/blink/config.h
+++ keyboards/dxkb/blink/config.h
@@ -2,3 +2,6 @@
 #define EE_HANDS
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
+
+#define OS_DETECTION_DEBOUNCE 250
+#define OS_DETECTION_KEYBOARD_RESET
